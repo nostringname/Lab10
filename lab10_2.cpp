a6e2347ccc8805ecafc68c5f930c80f246dce9f9
notmyname <ddsashdasdasda455465@gmail.com>
2022-12-27 15:21:19 +0700

    Done

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -2,6 +2,22 @@
 using namespace std;
 
 //Write the function printO() here
+string printO(int N,int M) {
+
+	if (N <= 0 || M <= 0) {
+		cout << "Invalid input";
+	}
+
+	for (int i=1;i<=N;i++) {
+
+		for (int j=1;j<=M;j++) {
+
+			cout << "O";
+		}
+		cout << "\n";
+	}
+	return "0";
+}
 
 int main(){
 	
